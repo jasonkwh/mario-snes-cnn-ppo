1ee97ade8240da64173a312b4e8719a937ecebc0
Jason Huang <jasonkwh@gmail.com>
2026-08-08 22:21:48 +1000

    refactor: update RewardWrapper to track lives instead of x
    - Replaced tracking of 'x' with 'lives' in the RewardWrapper class.
    - Implemented a death penalty in the reward system when a life is lost.
    - Removed debug print statements from the step method for cleaner output.

--- reward.py
+++ reward.py
@@ -3,28 +3,24 @@ import gymnasium as gym
 class RewardWrapper(gym.Wrapper):
     def __init__(self, env):
         super().__init__(env)
-        self.prev_x = 0
+        self.prev_lives = 0
 
     def reset(self, **kwargs):
         obs, info = self.env.reset(**kwargs)
-        self.prev_x = info.get("x", 0)
+        self.prev_lives = info.get("lives", 0)
         return obs, info
 
     def step(self, action):
         obs, reward, terminated, truncated, info = self.env.step(action)
 
-        print(obs)
-        print(reward)
-        print(terminated)
-        print(truncated)
-        print(info)
+        current_lives = info.get("lives", self.prev_lives)
 
-        current_x = info.get("x", 0)
-        x_reward = current_x - self.prev_x
-        self.prev_x = current_x
+        if self.is_lost_life(current_lives):
+            reward -= 15.0 # Death penalty
+            
+        self.prev_lives = current_lives
 
-        custom_reward = x_reward * 0.1
-        if terminated:
-            custom_reward -= 15.0  # Death penalty
+        return obs, reward, terminated, truncated, info
 
-        return obs, custom_reward, terminated, truncated, info
+    def is_lost_life(self, current_lives):
+        return current_lives < self.prev_lives

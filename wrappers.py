219c06eb1b6a6e261db932f6093d5560d8a50f39
Jason Huang <jasonkwh@gmail.com>
2026-08-09 00:15:14 +1000

    refactor: enhance RewardWrapper to track previous state variables
    - Updated RewardWrapper to track previous values of coins, score, and x position.
    - Adjusted reward calculation to include progress rewards based on x position changes.
    - Cleaned up the step method by removing unnecessary print statements and ensuring state variables are updated correctly.

--- wrappers.py
+++ wrappers.py
@@ -5,28 +5,31 @@ class RewardWrapper(gym.Wrapper):
     def __init__(self, env):
         super().__init__(env)
         self.prev_lives = 0
-        self.coins = 0
-        self.score = 0
+        self.prev_coins = 0
+        self.prev_score = 0
+        self.prev_x = 0
 
     def reset(self, **kwargs):
         obs, info = self.env.reset(**kwargs)
         self.prev_lives = info.get("lives", 0)
-        self.coins = info.get("coins", 0)
-        self.score = info.get("score", 0)
+        self.prev_coins = info.get("coins", 0)
+        self.prev_score = info.get("score", 0)
+        self.prev_x = info.get("x", 0)
+
         return obs, info
 
     def step(self, action):
         obs, reward, terminated, truncated, info = self.env.step(action)
 
-        print(f"reward: {reward}")
-        print(f"info: {info}")
-
         current_lives = info.get("lives", self.prev_lives)
-        current_coins = info.get("coins", self.coins)
-        current_score = info.get("score", self.score)
+        current_coins = info.get("coins", self.prev_coins)
+        current_score = info.get("score", self.prev_score)
+        current_x = info.get("x", self.prev_x)  
 
         if self.is_lost_life(current_lives):
             reward -= 5.0 # Death penalty
+        else:
+            reward += self.x_changed(current_x) * 0.01 # Progress reward/penalty
 
         if self.increase_score(current_score):
             if self.has_collected_coin(current_coins):
@@ -35,8 +38,12 @@ class RewardWrapper(gym.Wrapper):
                 reward += 0.05 # Score reward
 
         self.prev_lives = current_lives
-        self.coins = current_coins
-        self.score = current_score
+        self.prev_coins = current_coins
+        self.prev_score = current_score
+        self.prev_x = current_x
+
+        print(f"reward: {reward}")
+        print(f"info: {info}")
 
         return obs, reward, terminated, truncated, info
 
@@ -44,10 +51,13 @@ class RewardWrapper(gym.Wrapper):
         return current_lives < self.prev_lives
 
     def has_collected_coin(self, current_coins):
-        return current_coins > self.coins
+        return current_coins > self.prev_coins
 
     def increase_score(self, current_score):
-        return current_score > self.score
+        return current_score > self.prev_score
+
+    def x_changed(self, current_x):
+        return current_x - self.prev_x
 
 class ActionWrapper(gym.ActionWrapper):
     def __init__(self, env):
